63819d9a8501f49e1fc8652f06c2193d49cfab61
kiko <cahenriquesoares@gmail.com>
2023-03-14 16:10:54 -0300

    Voltado ao estado antes de tentar aplicar o decouple na aplicação. Motivo: erro no deploy do pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from ctypes import cast
 from pathlib import Path
-from decouple import config
+# from decouple import config
 from django import conf
 from traitlets import default
 
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-wi_b7+y6ph94(2%t-g(un^6sej%(&8cwm2)rv%h^s%t)&g9d&_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False,cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
