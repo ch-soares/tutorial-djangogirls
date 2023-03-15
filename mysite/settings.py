f19c47491770366b5f96ffcc3847dbcc0b152793
kiko <cahenriquesoares@gmail.com>
2023-03-14 21:40:00 -0300

    Configuração para uso do decouple no settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from ctypes import cast
 from pathlib import Path
 from django import conf
-
+from decouple import config
+from traitlets import default
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wi_b7+y6ph94(2%t-g(un^6sej%(&8cwm2)rv%h^s%t)&g9d&_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
